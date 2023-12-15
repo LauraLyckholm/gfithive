02a25d973409925b301de71d6cee93223ffebd18
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-12-15 07:37:53 +0100

    Can now display gifts, logout btn seems to bug though

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,6 +9,7 @@ export const Home = () => {
             <h1>Welcome to</h1>
             <img id="logo" src="/gifthive-logo.png" alt="Logo" />
             <Link to="/login"><Button className="primary" btnText="Get started" /></Link>
+            <p>Attempting to push</p>
         </>
     )
 }
