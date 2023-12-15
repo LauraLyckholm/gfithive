51f76f2907b7e433ead9b150b13dadf5890fafcc
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-12-15 07:50:12 +0100

    Attempting to deploy again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import routes from "./routes/routes";
-import { Header } from "./components/header/Header";
 import "./app.css";
 
 export const App = () => {
@@ -8,7 +7,6 @@ export const App = () => {
   return (
     // BrowserRouter is a wrapper component that makes the routing work
     <BrowserRouter>
-      <Header />
       <section className="main-wrapper">
         {/* <ScrollToTop /> */}
         <main className="main">
